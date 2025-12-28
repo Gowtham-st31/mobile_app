b9a45af19b1799f83e665d742c8aadc5d4eceb6b
Gowtham S T <gowthamst98438@gmail.com>
2025-12-28 17:44:50 +0530

    Update check_users.py

--- check_users.py
+++ check_users.py
@@ -3,7 +3,7 @@ from werkzeug.security import generate_password_hash
 from datetime import datetime
 import os # Import os for potential environment variables, though not used in MONGO_URI directly here
 
-MONGO_URI = os.getenv("MONGO_URI")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://gowthamst31:gowtham123@powerloom-cluster.gfl74dq.mongodb.net/?retryWrites=true&w=majority&appName=powerloom-cluster")
 
 
 if not MONGO_URI:
